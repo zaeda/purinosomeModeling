edafadc055f2d3d71705f50365785c94edbded32
Zaeda Blotner <zaedablotner@Zaedas-MacBook-Air.local>
2020-08-07 16:21:06 -0400

    numerical update

--- PduParams.m
+++ PduParams.m
@@ -6,7 +6,7 @@ classdef PduParams< matlab.mixin.SetGet
     % includes parameters for numerical integration -CMJ
     properties
         jc = 0;                 % active uptake rate of 1,2-PD(cm/s)                                        MCP: 0          human: 8.66*1e9
-        q = 1e-12;           % added, amt of R5P produced by RPIA (µmol/s)         8.08e-16                 MCP: 0          human: 8.66*1e9
+        q = 8.08e-16;           % added, amt of R5P produced by RPIA (µmol/s)         8.08e-16                 MCP: 0          human: 8.66*1e9
         kcA = 1e3;              % permeability of MCP to propanal (cm/s)                                    MCP: 1e-5       human: 1e3
         kcP = 1e3;              % permeability of MCP to 1,2-PD (cm/s)                                      MCP: 1e-5       human: 1e3
         Rb = 2e-4;              % radius of cell (cm)                                                       MCP: 5e-5       human: 1.05*1e-3
@@ -20,11 +20,11 @@ classdef PduParams< matlab.mixin.SetGet
         Pout = 0;               % uM concentration of 1,2-PD outside                                        MCP: 55*1e3     human: 5
         Aout = 0;               % uM concentration of propanal outside
 
-        kcatCDE = 1;         % rxns/s maximum reaction rate at single PduCDE active site                 MCP: 300        human: 4.24   
-        NCDE = 1e6;           % number of PduCDE active sites (updated based on MFS data)   e3              MCP: 1500       human: 3.38*1e5
+        kcatCDE = 6.27;         % rxns/s maximum reaction rate at single PduCDE active site                 MCP: 300        human: 4.24   
+        NCDE = 7.1e3;           % number of PduCDE active sites (updated based on MFS data)   e3              MCP: 1500       human: 3.38*1e5
         KCDE= 490;              % half max reaction rate of PduCDE, uM                                      MCP: 0.5*1e3    human: 490
-        kcatPQ = 1       % rxns/s maximum rate of aldehyde consumption by PduP/PduQ                  MCP: 55         human: 3.98e-3
-        NPQ = 1e6;         % number of PduP/PduQ active sites (updated based on MFS data)     e4         MCP: 2500       human: 1.76*1e6
+        kcatPQ = 9.67e-4       % rxns/s maximum rate of aldehyde consumption by PduP/PduQ                  MCP: 55         human: 3.98e-3
+        NPQ = 1.87e4;         % number of PduP/PduQ active sites (updated based on MFS data)     e4         MCP: 2500       human: 1.76*1e6
         KPQ = 480;              % uM half max reaction rate for PduP/PduQ                                   MCP: 15*1e3     human: 480
         
         x;
